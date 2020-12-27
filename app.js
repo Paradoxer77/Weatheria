092d3d80f23bf47847d2a4c723d6a46b6d615821
Sakshyam Chapagain <chapagainsakshyam@gmail.com>
2020-12-27 20:06:49 +0545

    Created fetch for the API call

---
+++ app.js
@@ -0,0 +1,10 @@
+async function getWeather(cityName) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=1df43d26385f44b8842140711202712&q=${cityName}`,
+    { mode: "cors" }
+  );
+
+  const weatherInfo = await response.json();
+}
+
+getWeather("london");
